e1533206d135caf06dc6559eaadbade1f8520bc4
unknown <nallediaz1905@gmail.com>
2024-02-27 10:47:18 -0600

    puse el 1er cambio

---
+++ gitprueba2/gitprueba2.java
@@ -0,0 +1,12 @@
+package gitprueba2;
+
+public class gitprueba2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Mi nombre es Nalle Diaz");
+
+	}
+
+}
